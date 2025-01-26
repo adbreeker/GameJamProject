ca3e483468e0a92034680408efdce7fb4604dd48
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-01-26 11:52:10 +0100

    gum grenade effects

---
+++ Assets/Scripts/Interactions/GumGrenadeController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GumGrenadeController : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rigidbody;
+    [SerializeField] LayerMask _collideMask;
+    [SerializeField] GameObject _impactPrefab;
+
+    float _explosionRadius;
+
+    public void ThrowInDirection(Vector3 direction, float throwForce, float explosionRadius)
+    {
+        _rigidbody.AddForce(direction * throwForce, ForceMode.Impulse);
+        _explosionRadius = explosionRadius;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if((_collideMask.value & (1 << collision.gameObject.layer)) != 0)
+        {
+            Instantiate(_impactPrefab, transform.position, Quaternion.identity);
+            Collider[] enemies = Physics.OverlapSphere(transform.position, _explosionRadius, LayerMask.GetMask("Enemy"));
+            foreach (Collider collider in enemies)
+            {
+                collider.gameObject.GetComponent<EnemyStateMachine>()?.StunEnemy();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
